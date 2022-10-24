38458fcd705112778d39b3116ed9c689ba01e95b
R-oss-7 <anotniososa.ruiz11@gmail.com>
2022-10-24 08:56:57 -0700

    Creamos un proyecto libreria y creamos un folder con entidades

---
+++ Noviembre.Core/Entidades/Cita.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noviembre.Core.Entidades
+{
+    public class Cita
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public Modulo Modulo { get; set; }
+        public Ciudadano Ciudadado { get; set; }
+
+    }
+}
---
+++ Noviembre.Core/Entidades/Municipio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noviembre.Core.Entidades
+{
+    public class Municipio
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public Estado Estado { get; set; }
+
+    }
+}
